35682a4c581929b266068f07638118e091811df3
남도현 <2020097010@hanyang.ac.kr>
2024-04-05 20:21:18 +0900

    autocomplete esc when not selected

--- .config/nvim/plug-config/cmp.lua
+++ .config/nvim/plug-config/cmp.lua
@@ -28,7 +28,13 @@ require('cmp').setup({
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
-	  ['<Esc>'] = cmp.mapping.abort(),
+	  ['<Esc>'] = cmp.mapping(function (fallback)
+		  if cmp.visible() and cmp.get_active_entry() then
+			  cmp.abort()
+		  else
+			  fallback()
+		  end
+	  end, {"i", "s"}),
 	  ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
