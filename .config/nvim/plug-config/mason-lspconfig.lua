fa200c2f11f98a7aa5d398b28b2af3a44606f35d
남도현 <2020097010@hanyang.ac.kr>
2025-08-08 19:15:21 +0900

    mason-lspconfig update

--- .config/nvim/plug-config/mason-lspconfig.lua
+++ .config/nvim/plug-config/mason-lspconfig.lua
@@ -2,12 +2,3 @@ require("mason-lspconfig").setup()
 
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
--- auto using mason
-require('mason-lspconfig').setup_handlers({
-  function (server)
-	  require('lspconfig')[server].setup {
-		capabilities = capabilities,
-	  }
-  end
-})
