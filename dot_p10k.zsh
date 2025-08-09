8ebe0bedc193819fce4f37c0f5089e26d8ee4a55
남도현 <2020097010@hanyang.ac.kr>
2025-08-09 19:32:56 +0900

    Update .p10k.zsh

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -546,7 +546,7 @@
   typeset -g POWERLEVEL9K_STATUS_ERROR_SIGNAL_FOREGROUND=160
   # Use terse signal names: "INT" instead of "SIGINT(2)".
   typeset -g POWERLEVEL9K_STATUS_VERBOSE_SIGNAME=false
-  typeset -g POWERLEVEL9K_STATUS_ERROR_SIGNAL_VISUAL_IDENTIFIER_EXPANSION='✘'
+  typeset -g POWERLEVEL9K_STATUS_ERROR_SIGNAL_VISUAL_IDENTIFIER_EXPANSION=''
 
   # Status when some part of a pipe command fails and the overall exit status is also non-zero.
   # It may look like this: 1|0.
