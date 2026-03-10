135a2835be8913565e07eed1c978a0a128a340bb
LegenDUST <dhnam0502@naver.com>
2026-03-10 21:57:39 +0900

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -6,7 +6,7 @@ Invoke-Expression (&starship init powershell)
 #f45873b3-b655-43a6-b217-97c00aa0db58
 
 # mise 활성화
-mise activate powershell | Out-String | Invoke-Expression
+mise activate pwsh | Out-String | Invoke-Expression
 
 # Alias 설정
 function eza-tree-wrapper { eza --tree --icons $args }
