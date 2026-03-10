53383794bf868bc27835d3ac449450f8bcadd746
LegenDUST <dhnam0502@naver.com>
2026-03-10 21:50:21 +0900

    Add Documents/PowerShell/Microsoft.PowerShell_profile.ps1

---
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,25 @@
+
+#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+Invoke-Expression (&starship init powershell)
+#f45873b3-b655-43a6-b217-97c00aa0db58
+
+# mise 활성화
+~/.local/bin/mise activate powershell | Out-String | Invoke-Expression
+
+# Alias 설정
+function eza-tree-wrapper { eza --tree --icons $args }
+Set-Alias ls eza
+Set-Alias tree eza-tree-wrapper
+Set-Alias find fd
+Set-Alias grep rg
+Set-Alias vi nvim
+Set-Alias vim nvim
+
+# FZF 설정 (파워셸용 fzf 모듈을 따로 설치해야 더 원활합니다)
+$env:FZF_DEFAULT_OPTS="--preview-window=right:60% --preview 'if (Test-Path -PathType Container {}) { eza --tree --level=2 --color=always {} } else { bat --style=numbers --color=always --line-range :500 {} }'"
+
+# zoxide 및 starship 초기화
+zoxide init --cmd cd powershell | Out-String | Invoke-Expression
+starship init powershell | Out-String | Invoke-Expression
