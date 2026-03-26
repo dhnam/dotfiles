41f48f51f01d26fcb9839c06c4da155c955c2069
LegenDUST <dhnam0502@naver.com>
2026-03-26 18:55:22 +0900

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,4 @@
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
 #f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
 
@@ -10,7 +11,8 @@ mise activate pwsh | Out-String | Invoke-Expression
 
 # Alias 설정
 function eza-tree-wrapper { eza --tree --icons $args }
-Set-Alias ls eza
+function eza-ls-wrapper {eza --icons $args}
+Set-Alias ls eza-ls-wrapper
 Set-Alias tree eza-tree-wrapper
 Set-Alias find fd
 Set-Alias grep rg
@@ -18,8 +20,10 @@ Set-Alias vi nvim
 Set-Alias vim nvim
 
 # FZF 설정 (파워셸용 fzf 모듈을 따로 설치해야 더 원활합니다)
-$env:FZF_DEFAULT_OPTS="--preview-window=right:60% --preview 'if (Test-Path -PathType Container {}) { eza --tree --level=2 --color=always {} } else { bat --style=numbers --color=always --line-range :500 {} }'"
+$env:FZF_DEFAULT_COMMAND = 'fd --type f --strip-cwd-prefix --hidden --follow --exclude .git'
+$env:FZF_DEFAULT_OPTS="--preview-window=right:60% --preview 'dir /a:d {} >nul 2>&1 && eza --tree --level=2 --color=always {} || bat --style=numbers --color=always --line-range :500 {}'"
 
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 # zoxide 및 starship 초기화
 starship init powershell | Out-String | Invoke-Expression
 Invoke-Expression (& { (zoxide init --cmd cd powershell | Out-String) })
